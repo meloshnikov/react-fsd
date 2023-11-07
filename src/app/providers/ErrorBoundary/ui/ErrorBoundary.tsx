b3fa67e2296432872da09453e97657116d862aa8
meloshnikov <meloshnikoff@gmail.com>
2023-11-07 22:26:18 +0300

    Adds error validation, Profile tests.

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -16,7 +16,6 @@ class ErrorBoundary
         this.state = { hasError: false };
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     static getDerivedStateFromError(error: Error) {
         // Update state so the next render will show the fallback UI.
         return { hasError: true };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 export type Mods = Record<string, boolean | string | undefined>
 
 export function classNames(
